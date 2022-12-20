60b0b27736b674ef5019852eb1abab72db5b1a39
azw4d <118949872+azw4d@users.noreply.github.com>
2022-12-20 13:29:34 +0600

    <3
    <3

---
+++ bot.js
@@ -0,0 +1,49 @@
+// Deploys the commands to Discord.
+require('/app/deploy-commands.js')
+
+
+// Discord clients and event execution.
+const fs = require('fs');
+const { Client, Collection, Intents } = require('discord.js');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands');
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.data.name, command);
+}
+
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const event = require(`./events/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
+
+// When bot is connected, create commands and reply to them when executed.
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		return interaction.reply({ content: 'Sorry, unable to execute command </3', ephemeral: true });
+	}
+});
+
+
+// Log into the bot. (Don't forget to specify token on the .env file.)
+client.login(process.env.token);
